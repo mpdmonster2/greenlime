ce5c7c2a6452c1690324029d50ff2cdfeaed358a
M1k3L1m3 <dennissheynkerman@gmail.com>
2019-09-20 22:00:44 -0500

    owo

--- templates/url_form.php
+++ templates/url_form.php
@@ -1,5 +1,5 @@
 <style>
-a.button1 {
+a.mikelimeb {
 padding:1.4em 1.4em;
 margin:0 0.3em 0.3em 0;
 text-decoration:none;
@@ -11,20 +11,20 @@ transition: all 0.2s;
 margin-top:8px;
 float:right;
 }
-a.button1:hover {
+a.mikelimeb:hover {
 color:#CD2769;
 }
 @media screen and (max-width:600px){
-a.button1{
+a.mikelimeb{
 display:none;
 }
 }
 
-.topnav {
+.mikelimenav {
 overflow: hidden;
 background-color: #34062C;
 }
-.topnav a {
+.mikelimenav a {
 float: left;
 display: block;
 color: #CD2769;
@@ -37,14 +37,14 @@ font-size: 17px;
 background-color: #4CAF50;
 color: white;
 }
-.topnav .icon {
+.mikelimenav .icon {
 display: none;
 }
-.dropdown {
+.mikelimedrop {
 float: left;
 overflow: hidden;
 }
-.dropdown .dropbtn {
+.mikelimedrop .mikelimedropmikelimebtncool {
 font-size: 15px;
 border: none;
 outline: none;
@@ -55,7 +55,7 @@ font-family: inherit;
 margin: 0;
 text-align: center;
 }
-.dropdown-content {
+.mikelimedrop-content {
 display: none;
 position: absolute;
 background-color: #f9f9f9;
@@ -63,7 +63,7 @@ min-width: 160px;
 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
 z-index: 1;
 }
-.dropdown-content a {
+.mikelimedrop-content a {
 float: none;
 color: black;
 padding: 22px 7%;
@@ -71,48 +71,48 @@ text-decoration: none;
 display: block;
 text-align: left;
 }
-.topnav a:hover, .dropdown:hover .dropbtn {
+.mikelimenav a:hover, .mikelimedrop:hover .mikelimedropmikelimebtncool {
 background-color: #4286f4;
 color: white;
 }
-.dropdown-content a:hover {
+.mikelimedrop-content a:hover {
 background-color: #ddd;
 color: black;
 }
-.dropdown:hover .dropdown-content {
+.mikelimedrop:hover .mikelimedrop-content {
 display: block;
 }
 @media screen and (min-width: 600px) {
-.topnav a:not(:first-child), .dropdown .dropbtn {
+.mikelimenav a:not(:first-child), .mikelimedrop .mikelimedropmikelimebtncool {
 display: none;
 }
-.topnav a.icon {
+.mikelimenav a.icon {
 float: right;
 display: block;
 }
 }
 @media screen and (min-width: 600px) {
-.topnav.responsive {position: relative;}
-.topnav.responsive .icon {
+.mikelimenav.responsive {position: relative;}
+.mikelimenav.responsive .icon {
 position: absolute;
 right: 7%;
 top: 0;
 }
-.topnav.responsive a {
+.mikelimenav.responsive a {
 float: none;
 display: block;
 text-align: left;
 }
-.topnav.responsive .dropdown {float: none;}
-.topnav.responsive .dropdown-content {position: relative;}
-.topnav.responsive .dropdown .dropbtn {
+.mikelimenav.responsive .mikelimedrop {float: none;}
+.mikelimenav.responsive .mikelimedrop-content {position: relative;}
+.mikelimenav.responsive .mikelimedrop .mikelimedropmikelimebtncool {
 display: block;
 width: 100%;
 text-align: left;
 }
 }
 
-.btn {
+.mikelimebtncool {
 box-sizing: border-box;
 background-color: transparent;
 border: 2px solid #CD2769;
@@ -135,7 +135,7 @@ text-transform: uppercase;
 font-family: 'Montserrat', Sans-Serif;
 font-weight: 700;
 }
-.btn:hover, .btn:focus {
+.mikelimebtncool:hover, .mikelimebtncool:focus {
 color: #fff;
 outline: 0;
 }
@@ -151,7 +151,7 @@ transition: background 300ms ease-in-out;
 background-position: 0;
 }
 
-.btn2 {
+.mikelimebtncool2 {
 box-sizing: border-box;
 background-color: transparent;
 border: 2px solid #CD2769;
@@ -174,7 +174,7 @@ text-transform: uppercase;
 font-family: 'Montserrat', Sans-Serif;
 font-weight: 700;
 }
-.btn2:hover, .btn2:focus {
+.mikelimebtncool2:hover, .mikelimebtncool2:focus {
 color: #fff;
 outline: 0;
 }
@@ -190,7 +190,7 @@ transition: background 300ms ease-in-out;
 background-position: 0;
 }
 
-.topFixedBar
+.mikelimebartop
 {
 background-color: #120a21;
 height: 67px;
@@ -207,26 +207,26 @@ font-family: comfort;
 src: url(Comfortaa.ttf);
 }
 
-.text {
+.mikelimetext {
 font-size: 48px;
 font-family: title;
 } @media (max-width:600px) {
-.text {
+.mikelimetext {
 font-size: 40px;
 font-family: title;
 }
 }
-.textwidth {
+.mikelimetextwidth {
 width:43%;
 font-size: 20px;
 }
 @media (max-width:600px) {
-.textwidth {
+.mikelimetextwidth {
 width:85%;
 }
 }
 
-.leftText {
+.mikelimeleftText {
   margin-left:7%;
   margin-top:17px;
   float:left;
@@ -236,32 +236,32 @@ width:85%;
   font-size:20px;
   font-family:'comfort',sans-serif;;
 }
- .leftText:hover, .leftText:active {
+ .mikelimeleftText:hover, .mikelimeleftText:active {
   text-decoration: none;
   color: #FFFFFF;
 }
 @media (max-width:600px) {
-.leftText {
+.mikelimeleftText {
   display:none;
 }
 }
 
 </style>
 
-		<div class="topFixedBar" style="vertical-align:top;" style="position:fixed;">
+		<div class="mikelimebartop" style="vertical-align:top;" style="position:fixed;">
 			 <div align="left">
-					<a href="/" class="leftText">MikeLime</a>
+					<a href="/" class="mikelimeleftText">MikeLime</a>
 			 </div>
 			 <div align="right">
-					<a href="/selection.html" class="button1">Surf Freely</a>
-					<a href="/gs.html" class="button1">Play Games</a>
-					<a id="movie" class="button1">Watch Movies</a>
-					<a href="/chatbox.html" class="button1">Chatbox</a>
-					<div class="dropdown" style="text-align:center !important;">
-						 <button class="dropbtn">MikeLime
+					<a href="/selection.html" class="mikelimeb">Surf Freely</a>
+					<a href="/gs.html" class="mikelimeb">Play Games</a>
+					<a id="movie" class="mikelimeb">Watch Movies</a>
+					<a href="/chatbox.html" class="mikelimeb">Chatbox</a>
+					<div class="mikelimedrop" style="text-align:center !important;">
+						 <button class="mikelimedropmikelimebtncool">MikeLime
 						 <i class="fa fa-caret-down"></i>
 						 </button>
-						 <div class="dropdown-content">
+						 <div class="mikelimedrop-content">
 								<a href="/selection.html">Surf Freely</a>
 								<a href="/gs.html">Play Games</a>
 								<a id="movie">Watch Movies</a>
