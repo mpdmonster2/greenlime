2eb01d39bc84592f8b7a7939e09e1f8a05bb6dcd
M1k3L1m3 <dennissheynkerman@gmail.com>
2019-09-20 21:13:51 -0500

    owo

--- templates/url_form.php
+++ templates/url_form.php
@@ -255,59 +255,6 @@ width:85%;
 
 </style>
 
-<style>
-#top_form {
-	position: fixed;
-	top:0;
-	left:0;
-	width: 100%;
-
-	margin:0;
-
-	z-index: 2100000000;
-	-moz-user-select: none;
-	-khtml-user-select: none;
-	-webkit-user-select: none;
-	-o-user-select: none;
-
-	border-bottom:1px solid #151515;
-
-    background:#FFC8C8;
-
-	height:45px;
-	line-height:45px;
-}
-
-#top_form input[name=url] {
-	width: 550px;
-	height: 20px;
-	padding: 5px;
-	font: 13px "Helvetica Neue",Helvetica,Arial,sans-serif;
-	border: 0px none;
-	background: none repeat scroll 0% 0% #FFF;
-}
-
-</style>
-
-<script>
-var url_text_selected = false;
-
-function smart_select(ele){
-
-	ele.onblur = function(){
-		url_text_selected = false;
-	};
-
-	ele.onclick = function(){
-		if(url_text_selected == false){
-			this.focus();
-			this.select();
-			url_text_selected = true;
-		}
-	};
-}
-</script>
-
 		<div class="topFixedBar" style="vertical-align:top;">
 			 <div align="left">
 					<a href="/" class="leftText">MikeLime</a>
